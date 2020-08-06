64bc9bbb62c1b28312bff7177e62c306e737f6ae
Nikolay Tarasenko <kolyatarasenko@ukr.net>
2020-08-07 01:37:49 +0300

    mobile version was implemented

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -1,15 +1,23 @@
-document.addEventListener('DOMContentLoaded', e => {
+document.addEventListener('DOMContentLoaded', () => {
     const shareButtons = document.querySelectorAll('.article__share-btn');
     const moreButton = document.querySelector('.more__btn');
     const popupList = document.querySelector('.more__popup-list');
+    const nav = document.querySelector('.nav');
+    const hamburgerButton = document.querySelector('.hamburger');
+    const navLinks = document.querySelectorAll('.nav__item');
 
-    // add event listeners to buttons
+    // add event listeners to buttons and links
     for (let i = 0; i < shareButtons.length; i++) {
         shareButtons[i].addEventListener('click', showSharedContent);
         shareButtons[i].addEventListener('click', showStats);
     }
 
+    for (let i = 0; i < navLinks.length; i++) {
+        navLinks[i].addEventListener('click', setActiveLink);
+    }
+
     moreButton.addEventListener('click', toggleMorePopup);
+    hamburgerButton.addEventListener('click', toggleMobileMenu);
 
     // close more popup list if clicked outside of it
     document.addEventListener('click', e => {
@@ -18,6 +26,17 @@ document.addEventListener('DOMContentLoaded', e => {
         }
     });
 
+    // reset menu state when screen width is more than 769 pixels
+    window.addEventListener('resize', e => {
+        const width = document.documentElement.clientWidth || screen.width;
+
+        if (width > 768) {
+            hamburgerButton.classList.remove('active');
+            nav.classList.remove('active');
+        }
+    });
+
+    // handlers
     function showSharedContent(e) {
         const articleNode = e.target.closest('.article');
         const sharedContentNode = articleNode.querySelector('.article__shared-content');
@@ -36,6 +55,21 @@ document.addEventListener('DOMContentLoaded', e => {
 
     function toggleMorePopup(e) {
         popupList.classList.toggle('active');
+    }
+
+    function toggleMobileMenu(e) {
+        hamburgerButton.classList.toggle('active');
+        nav.classList.toggle('active');
+    }
+
+    function setActiveLink(e) {
+        const currentLink = e.target;
+
+        for (let i = 0; i < navLinks.length; i++) {
+            navLinks[i].classList.remove('nav__item--active');
+        }
+
+        currentLink.classList.add('nav__item--active');
     }
 });
 
