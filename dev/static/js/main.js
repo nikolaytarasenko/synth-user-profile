d68d398b20a4b167fdbb008d5afbd320acf1eed8
Nikolay Tarasenko <kolyatarasenko@ukr.net>
2020-08-06 22:20:02 +0300

    desktop version was implemented

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -1,13 +1,49 @@
-$(document).ready(function () {
-    svg4everybody({});
+document.addEventListener('DOMContentLoaded', e => {
+    const shareButtons = document.querySelectorAll('.article__share-btn');
+    const moreButton = document.querySelector('.more__btn');
+    const popupList = document.querySelector('.more__popup-list');
+
+    // add event listeners to buttons
+    for (let i = 0; i < shareButtons.length; i++) {
+        shareButtons[i].addEventListener('click', showSharedContent);
+        shareButtons[i].addEventListener('click', showStats);
+    }
+
+    moreButton.addEventListener('click', toggleMorePopup);
+
+    // close more popup list if clicked outside of it
+    document.addEventListener('click', e => {
+        if (!e.target.closest('.user__more')) {
+            popupList.classList.remove('active');
+        }
+    });
+
+    function showSharedContent(e) {
+        const articleNode = e.target.closest('.article');
+        const sharedContentNode = articleNode.querySelector('.article__shared-content');
+
+        sharedContentNode.classList.add('show');
+    }
+
+    function showStats(e) {
+        const shareButton = e.target;
+        const shareButtonWrapper = shareButton.parentElement;
+        const currentStatsBlock = shareButtonWrapper.nextElementSibling;
+
+        shareButtonWrapper.style.display = 'none';
+        currentStatsBlock.classList.add('show');
+    }
+
+    function toggleMorePopup(e) {
+        popupList.classList.toggle('active');
+    }
 });
 
 
-// Полифилы
+// Polyfills
 
 // forEach IE 11
 if ('NodeList' in window && !NodeList.prototype.forEach) {
-    console.info('polyfill for IE11');
     NodeList.prototype.forEach = function (callback, thisArg) {
         thisArg = thisArg || window;
         for (var i = 0; i < this.length; i++) {
@@ -39,11 +75,3 @@ if ('NodeList' in window && !NodeList.prototype.forEach) {
             Element.prototype.msMatchesSelector;
     }
 })();
-
-//Array.form IE 11
-if (!Array.from) {
-    Array.from = function (object) {
-        'use strict';
-        return [].slice.call(object);
-    };
-}
